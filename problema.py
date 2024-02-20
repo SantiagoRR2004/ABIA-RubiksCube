ec2dae6c2f171b4b404e6b65730c1fdfc30df2e7
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-02-20 20:16:51 +0100

    Added type hints to problema

--- problema.py
+++ problema.py
@@ -1,44 +1,50 @@
 # Interfaz genérico para los estados del espacio de estados
-from abc import abstractmethod
-from abc import ABCMeta
+from abc import abstractmethod, ABCMeta
+from typing import TypeVar, Any, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from busqueda import Busqueda
+
+O = TypeVar("O", bound="Operador")
+E = TypeVar("E", bound="Estado")
 
 
 class Estado(metaclass=ABCMeta):
     # Devuelve el Vector con la lista de Operadores aplicables sobre este Estado
     @abstractmethod
-    def operadoresAplicables(self):
+    def operadoresAplicables(self) -> list[O]:
         pass
 
     # Indica si este es un estado final (solución)
     @abstractmethod
-    def esFinal(self):
+    def esFinal(self) -> bool:
         pass
 
     @abstractmethod
     # Genera un nuevo Estado resultante de aplicar el Operador indicado
-    def aplicarOperador(self, operador):
+    def aplicarOperador(self, operador: O) -> E:
         pass
 
 
 # Interfaz para encapsular operadores
 class Operador(metaclass=ABCMeta):
     @abstractmethod
-    def getEtiqueta(self):
+    def getEtiqueta(self) -> Any:
         pass
 
     @abstractmethod
-    def getCoste(self):
+    def getCoste(self) -> int:
         pass
 
 
 # Clase genérica (indepeniente de estados y algoritmos concretos) que representa un problema de búsqueda en espacio de estados.
 # Está caracterizado por un Estado inicial y un método de Busqueda
 class Problema:
-    def __init__(self, inicial, buscador):
+    def __init__(self, inicial: E, buscador: "Busqueda") -> None:
         self.inicial = inicial
         self.buscador = buscador
 
     # Aplica el método de Busqueda de este Problema concreto para resolverlo.
     # Devuelve la lista de Operadores que permiten alcanzar un Estado final desde el Estado inicial del Problema
-    def obtenerSolucion(self):
+    def obtenerSolucion(self) -> list["Operador"]:
         return self.buscador.buscarSolucion(self.inicial)
