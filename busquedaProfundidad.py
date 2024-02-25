562c0b49be01a890465323727dc5b8d393c33bdd
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-02-25 19:52:08 +0100

    Refactor depth-first search implementation in BusquedaProfundidad class

--- busquedaProfundidad.py
+++ busquedaProfundidad.py
@@ -1,48 +1,67 @@
 from nodos import NodoAnchura
 from busqueda import Busqueda
 import time
+import traceback
 
 
 class BusquedaProfundidad(Busqueda):
+    # https://en.wikipedia.org/wiki/Depth-first_search
 
-    # Implementa la búsqueda en profundidad. Si encuentra solución recupera la lista de Operadores empleados almacenada en los atributos de los objetos NodoProfundidad
+    def dfs(self, node: NodoAnchura, visited: set[NodoAnchura]) -> NodoAnchura:
+        """
+        This is a recursive implementation of the depth first search
+
+        Args:
+            -node: NodoAnchura. The node to start the search from
+            -visited: set[NodoAnchura]. The set of visited nodes
+
+        Returns:
+            -NodoAnchura. The node that is the solution
+                or None if the solution is not found in the time limit
+        """
+        visited.add(node.estado.cubo.visualizar())
+
+        if node.estado.esFinal():
+            return node
+
+        elif time.time() - self.tiempoInicio > self.timeAmount:
+            return None
+
+        if len(traceback.extract_stack()) > 100:
+            """
+            This is not supposed to be in a dfs
+            but we use it to avoid:
+            RecursionError: maximum recursion depth exceeded in comparison
+            This should be used properly
+            in the limited depth search
+            """
+            return None
+
+        for operator in node.estado.operadoresAplicables():
+            hijo = node.estado.aplicarOperador(operator)
+            if hijo.cubo.visualizar() not in visited:
+                result = self.dfs(NodoAnchura(hijo, node, operator), visited)
+                if result or (time.time() - self.tiempoInicio > self.timeAmount):
+                    return result
+        return None
+
+    # Implementa la búsqueda en profundidad.
+    # Si encuentra solución recupera la lista de Operadores empleados
+    # almacenada en los atributos de los objetos NodoProfundidad
     def solveProblem(self):
-        nodoActual = None
-        actual, hijo = None, None
-        solutionFlag = False
-        abiertos = []
-        cerrados = dict()
-        abiertos.append(NodoAnchura(self.inicial, None, None))
+        cerrados = set()
+        cerrados.add(NodoAnchura(self.inicial, None, None))
 
-        while (
-            not solutionFlag
-            and len(abiertos) > 0
-            and time.time() - self.tiempoInicio < self.timeAmount
-        ):
-            if len(set(abiertos)) != len(abiertos):
-                print("Es necesario")
-            nodoActual = abiertos.pop(-1)
-            actual = nodoActual.estado
-            if actual.esFinal():
-                solutionFlag = True
-            else:
-                # cerrados[actual.cubo.visualizar()] = nodoActual
-                for operador in actual.operadoresAplicables():
-                    hijo = actual.aplicarOperador(operador)
-                    if hijo.cubo.visualizar() not in cerrados.keys():
-                        abiertos.append(NodoAnchura(hijo, nodoActual, operador))
-                        cerrados[hijo.cubo.visualizar()] = (
-                            hijo  # utilizamos CERRADOS para mantener también traza de los nodos añadidos a ABIERTOS
-                        )
+        solution = self.dfs(NodoAnchura(self.inicial, None, None), cerrados)
 
         toret = {
-            "lenOpened": len(abiertos),
+            "lenOpened": 0,
             "lenClosed": len(cerrados),
         }
 
-        if solutionFlag:
+        if solution:
             toret["solution"] = []
-            nodo = nodoActual
+            nodo = solution
             while nodo.padre != None:  # Asciende hasta la raíz
                 toret["solution"].insert(0, nodo.operador)
                 nodo = nodo.padre
