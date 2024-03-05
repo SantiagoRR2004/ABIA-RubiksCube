232600765bd6ac2c949a2e64252987e830ee1808
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-03-05 12:05:10 +0100

    Added best first search

---
+++ busquedaVoraz.py
@@ -0,0 +1,73 @@
+from nodos import NodoInformado
+from busqueda import Busqueda
+import time
+from cubo import Cubo
+from problemaRubik import EstadoRubik
+
+
+class BusquedaVoraz(Busqueda):
+    # https://en.wikipedia.org/wiki/Best-first_search
+
+    def __init__(self, heuristic):
+        self.heuristic = heuristic
+
+    def solveProblem(self):
+        bestValue = self.heuristic(EstadoRubik(Cubo()))
+        solutionFlag = False
+        abiertos = []
+        cerrados = set()
+        abiertos.append(
+            NodoInformado(
+                self.inicial,
+                None,
+                None,
+                0,
+                abs(self.heuristic(self.inicial) - bestValue),
+            )
+        )
+
+        while (
+            not solutionFlag
+            and len(abiertos) > 0
+            and time.time() - self.tiempoInicio < self.timeAmount
+        ):
+            nodoActual = min(abiertos, key=lambda x: x.getHeuristica())
+            abiertos.remove(nodoActual)
+
+            if nodoActual.estado.esFinal():
+                solutionFlag = True
+            else:
+
+                for operador in nodoActual.estado.operadoresAplicables():
+                    hijo = nodoActual.estado.aplicarOperador(operador)
+
+                    if hijo.cubo.visualizar() not in cerrados:
+
+                        abiertos.append(
+                            NodoInformado(
+                                hijo,
+                                nodoActual,
+                                operador,
+                                0,
+                                abs(self.heuristic(hijo) - bestValue),
+                            )
+                        )
+                        cerrados.add(hijo.cubo.visualizar())
+
+        toret = {
+            "lenOpened": len(abiertos),
+            "lenClosed": max(0, len(cerrados) - len(abiertos)),
+        }
+
+        if solutionFlag:
+            toret["solution"] = []
+            nodo = nodoActual
+            while nodo.padre != None:  # Asciende hasta la raíz
+                toret["solution"].insert(0, nodo.operador)
+                nodo = nodo.padre
+            toret["lenSolution"] = len(toret["solution"])
+        else:
+            toret["solution"] = None
+            toret["lenSolution"] = float("inf")
+
+        return toret
--- nodos.py
+++ nodos.py
@@ -22,3 +22,37 @@ class NodoNoInformado(Nodo):
     def __init__(self, estado: "Estado", padre: "Nodo", operador: "Operador") -> None:
         super().__init__(estado, padre)
         self.operador = operador
+
+
+class NodoInformado(NodoNoInformado):
+
+    def __init__(
+        self,
+        estado: "Estado",
+        padre: "Nodo",
+        operador: "Operador",
+        coste: float = 0,
+        heuristica: float = 0,
+    ) -> None:
+        """
+        Args:
+            -estado: Estado. The state of the problem
+            -padre: Nodo. The parent of the node
+            -operador: Operador. The operator used to generate the state
+            -coste: float. The cost of the node
+            -heuristica: float. The heuristic of the node
+
+        Returns:
+            -None
+        """
+        super().__init__(estado, padre, operador)
+        self.coste = coste
+        self.heuristica = heuristica
+        self.total = coste + heuristica
+
+    def getHeuristica(self) -> float:
+        """
+        Returns:
+            -float. The heuristic of the node
+        """
+        return self.heuristica
