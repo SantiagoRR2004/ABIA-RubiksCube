8a9c7d24eccc61471ca43fc0a7ae6f08071001b8
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-03-31 10:32:16 +0200

    Update Rubik's cube scoring calculation

--- problemaRubik.py
+++ problemaRubik.py
@@ -108,7 +108,7 @@ class EstadoRubik(Estado):
                     ):
                         n += 1
 
-        return n/18
+        return n / 20
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, EstadoRubik):
